fc045fd291dee127755cf15e4d7c11f39c3fa9cd
Abdu Malikh <106543950+abdumalikh44@users.noreply.github.com>
2023-11-23 15:21:19 +0700

    Add files via upload

---
+++ Basic Join/African Cities.sql
@@ -0,0 +1,6 @@
+/*Given the CITY and COUNTRY tables, query the names of all cities where the 
+CONTINENT is 'Africa'.*/
+SELECT A.NAME
+FROM CITY AS A
+INNER JOIN COUNTRY AS B ON A.COUNTRYCODE = B.CODE
+WHERE B.CONTINENT = 'Africa'
